fac02da65b55aad2f721bb1145d091d6573ee2ea
Zange10 <niklaszan@protonmail.com>
2026-07-14 13:07:21 +0200

    Determining line intersections between two line arrays

--- include/geometrylib_linetool.h
+++ include/geometrylib_linetool.h
@@ -21,5 +21,20 @@
 double interpolate_from_sorted_data_array2(DataArray2 *data_array, double x);
 
 
+/*
+ * ------------------------------------
+ * Intersections
+ * ------------------------------------
+ */
+
+/**
+ * @brief Returns all intersections between two line arrays
+ *
+ * @param line0 Pointer to the first 2-dimensional array of (x, y) pairs (x is sorted)
+ * @param line1 Pointer to the second 2-dimensional array of (x, y) pairs (x is sorted)
+ * @return Array of intersections
+ */
+DataArray2 * get_line_intersections(DataArray2 *line0, DataArray2 *line1);
+
 
 #endif //KMAT_GEOMETRYLIB_LINETOOL_H
--- src/linetool.c
+++ src/linetool.c
@@ -20,3 +20,35 @@ double interpolate_from_sorted_data_array2(DataArray2 *data_array, double x) {
 	double m = (p1.y-p0.y) / (p1.x-p0.x);
 	return (x-p0.x) * m + p0.y;
 }
+
+DataArray2 * get_line_intersections(DataArray2 *line0, DataArray2 *line1) {
+	size_t size0 = data_array2_size(line0);
+	size_t size1 = data_array2_size(line1);
+	DataArray2 *inters_points = data_array2_create();
+
+	if(size0 == 0 || size1 == 0) return inters_points;
+
+	int init_j = 0;
+
+	for(int i = 0; i < size0-1; i++) {
+		for(int j = init_j; j < size1-1; j++) {
+			if(are_line_segments_intersecting2(line0->data[i], line0->data[i+1], line1->data[j], line1->data[j+1])) {
+				Vector2 u0 = line0->data[i];
+				Vector2 u1 = line0->data[i+1];
+				Vector2 v0 = line1->data[j];
+				Vector2 v1 = line1->data[j+1];
+
+				double m0 = (u1.y - u0.y)/(u1.x - u0.x);
+				double m1 = (v1.y - v0.y)/(v1.x - v0.x);
+
+				double n0 = u0.y - m0*u0.x;
+				double n1 = v0.y - m1*v0.x;
+				double x_inters = (n0-n1)/(m1-m0);
+				double y_inters = m0*x_inters + n0;
+				data_array2_append_new(inters_points, vec2(x_inters, y_inters));
+			}
+		}
+	}
+
+	return inters_points;
+}
